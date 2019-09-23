473cdefcd9ab093694f602667a5312555baf9d80
asd0999 <obelixotic@gmail.com>
2019-09-22 20:17:02 -0400

    working with add/del/rename/sync_on_first_time capability

--- week3/server.js
+++ week3/server.js
@@ -1,32 +1,143 @@
+//fs over remote server with add/del/rename and sync on first run capability
 const {
   exec
 } = require('child_process');
 const fs = require('fs');
-const watchfile = '123';
+const watchdir = './public';
+let files_original;
+let files_rename;
+let mv_arg1, mv_arg2;
+let everything = '*';
 
-console.log(`Watching for file changes on ${watchfile}`);
+console.log(`Watching for file changes on ${watchdir}`);
 
-fs.watch(watchfile, (event, filename) => {
+fs.readdir(watchdir, function(err, items) {
+  files_original = items;
+  console.log('File list: ' + files_original);
+  scpFile(everything)
+});
+
+fs.watch(watchdir, (event, filename) => {
   if (filename) {
-    // console.log(event, filename);
-    console.log(`${filename} file Changed`);
-    scpFile(filename)
+    console.log(`${event}: ${filename}`);
+    if (event == 'change') {
+      scpFile(filename);
+    } else if (event == 'rename') {
+      fs.readdir(watchdir, function(err, items) {
+        files_rename = items;
+        // console.log(files_rename);
+        if (files_original.length < files_rename.length) {
+          //file added => scp
+          for (file of files_rename) {
+            console.log(file);
+            if (!files_original.includes(file)) {
+              scpFile(file);
+            }
+          }
+        } else if (files_original.length > files_rename.length) {
+          //file deleted => unlink
+          for (file of files_original) {
+            console.log(file);
+            if (!files_rename.includes(file)) {
+              delFile(file);
+            }
+          }
+        } else if (files_original.length == files_rename.length) {
+          //file renamed => scp + unlink
+          for (file of files_original) {
+            console.log(file);
+            if (!files_rename.includes(file)) {
+              mv_arg1 = file;
+            }
+          }
+
+          for (file of files_rename) {
+            console.log(file);
+            if (!files_original.includes(file)) {
+              mv_arg2 = file
+            }
+          }
+
+          mvFile(mv_arg1, mv_arg2);
+        }
+
+        files_original = files_rename;
+        console.log('updated file list: ' + files_original);
+      });
+    }
   }
 });
 
+
 function scpFile(filename) {
-  exec(`scp ${filename} root@198.211.97.177:/root/live_web/`,
+  exec(`scp ./${watchdir}/${filename} root@198.211.97.177:/root/live_web/public/`,
     (error, stdout, stderr) => {
 
       if (error) {
         console.log(`There was an error ${error}`);
+      } else {
+        console.log('updated file sent');
       }
 
-      console.log(`The stdout is ${stdout}`);
-      console.log(`The stderr is ${stderr}`);
+      // console.log(`The stdout is ${stdout}`);
+      // console.log(`The stderr is ${stderr}`);
     });
 }
 
+function delFile(filename) {
+  exec(`ssh root@198.211.97.177 "rm /root/live_web/public/${filename}"`,
+    (error, stdout, stderr) => {
+
+      if (error) {
+        console.log(`There was an error ${error}`);
+      } else {
+        console.log('updated file sent');
+      }
+    });
+}
+
+function mvFile(mv1, mv2) {
+  exec(`ssh root@198.211.97.177 "mv /root/live_web/public/${mv1} /root/live_web/public/${mv2}"`,
+    (error, stdout, stderr) => {
+
+      if (error) {
+        console.log(`There was an error ${error}`);
+      } else {
+        console.log('updated file sent');
+      }
+    });
+}
+
+
+//fs over remote server - simple scp
+// const {
+//   exec
+// } = require('child_process');
+// const fs = require('fs');
+// const watchfile = '123';
+//
+// console.log(`Watching for file changes on ${watchfile}`);
+//
+// fs.watch(watchfile, (event, filename) => {
+//   if (filename) {
+//     // console.log(event, filename);
+//     console.log(`${filename} file Changed`);
+//     scpFile(filename)
+//   }
+// });
+//
+// function scpFile(filename) {
+//   exec(`scp ${filename} root@198.211.97.177:/root/live_web/`,
+//     (error, stdout, stderr) => {
+//
+//       if (error) {
+//         console.log(`There was an error ${error}`);
+//       }
+//
+//       console.log(`The stdout is ${stdout}`);
+//       console.log(`The stderr is ${stderr}`);
+//     });
+// }
 
 
 
