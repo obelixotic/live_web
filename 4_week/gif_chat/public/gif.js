830d11cf235c6622f777490cfc92746eb9d41327
asd0999 <obelixotic@gmail.com>
2019-10-01 11:23:48 -0400

    working code, phase IV

---
+++ 4_week/gif_chat/public/gif.js
@@ -0,0 +1,213 @@
+let gif = [];
+let socket = io.connect();
+let start = null;
+let index = 0;
+let latest;
+
+
+//FROM PREVIOUS SKETCH FOR SOCKET ROOMS + USERNAME
+let otherUser = '';
+let inputText;
+
+//listen for event 'connect'
+socket.on('connect', function() {
+  console.log("Connected");
+});
+
+//listen for position
+socket.on('position', function(data) {
+  //console.log(data);
+});
+
+//receive from any event
+socket.on('news', function(data) {
+  console.log(data);
+});
+
+//recieve 'connectedUsername' data if partner is connected
+socket.on('connectedUsername', function(user) {
+  otherUser = user;
+  console.log('connected to: ' + otherUser);
+  // removeElements();
+  displayUsername(`You're connected to ${otherUser}`);
+});
+
+// GIF
+socket.on('image', function(imageData) {
+  //incoming image..
+  console.log("image recd");
+  // console.log("imageData.image: " + imageData.image);
+  latest = 0;
+  latest = imageData.image;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    //console.log("entered function, index: " + index);
+    if (!start) start = timestamp;
+    let progress = timestamp - start;
+
+    //console.log("progress: " + progress);
+
+    //if progress is greater than 0 than change otherimg.src to be the latest
+    if (progress > 200 * index) {
+      console.log('index: ' + index);
+      // console.log('latest index: ' + latest[index]);
+      document.getElementById('otherimage').src = latest[index];
+
+      if (index < latest.length) {
+        index++;
+      } else if (index == latest.length) {
+        index = 0;
+        start = timestamp;
+      }
+    }
+    window.requestAnimationFrame(step);
+  }
+});
+
+// DISPLAY USERNAME
+function displayUsername(data) {
+  // removeElements();
+  document.getElementById('usernameText').innerHTML = data;
+}
+
+function displayImage(data) {
+  // removeElements();
+  document.getElementById('partner-canvas').innerHTML = data;
+}
+
+
+////////////  ONCE PG LOADED  //////////////
+window.addEventListener('load', function() {
+
+  //SOCKET ROOM CONNECTION
+
+  //USERNAME
+  username = document.getElementById('username');
+  username.addEventListener('keypress', keyPressedUser);
+
+  socket.on('connectedUsername', function(user) {
+    otherUser = user;
+    console.log('connected to: ' + otherUser);
+    // removeElements();
+    displayUsername(`You're connected to ${otherUser}`);
+  });
+
+  function keyPressedUser(e) {
+    // console.log(e);
+    if (e.keyCode == 13) {
+      console.log('my username: ' + username.value);
+      let myUsername = username.value;
+      socket.emit('username', myUsername);
+
+      inputText = document.getElementById('inputText');
+      inputText.style.visibility = "visible";
+      inputText.innerHTML = myUsername;
+    }
+  }
+
+  //constrains - what do we want?
+  let constraints = {
+    audio: true,
+    video: true
+  }
+
+  //promp the user for permission to get the stream
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+
+      //attach to our video object
+      video.srcObject = stream;
+
+      //wait for the stream to load enough to play
+      video.onloadedmetadata = function(e) {
+        video.play();
+      };
+    })
+
+    .catch(function(err) {
+      /* handle the error */
+      alert(err);
+    });
+
+  ///////////// VIDEO + DOM SECTION //////////
+
+  //the video element on the page to display the webcam
+  let video = document.getElementById('myvideo');
+
+  let usernameText = document.getElementById('usernameText')
+  let playBttn = document.getElementById('playbttn');
+  let photoBttn = document.getElementById('photobttn');
+  let canvas = document.getElementById('mycanvas');
+  let context = canvas.getContext('2d');
+
+
+  context.fillStyle = "#FF000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+  photoBttn.addEventListener('click', function(e) {
+    for (i = 0; i < 10; i++) {
+      setTimeout(function() {
+        context.drawImage(video, 10, 10);
+        gif.push(canvas.toDataURL("image/jpeg"));
+      }, 200 * i);
+    }
+    // context.drawImage(video, 10, 10);
+    //console.log(canvas.toDataURL("image/jpeg"));
+    // gif.push(canvas.toDataURL("image/jpeg"));
+  });
+
+  playBttn.addEventListener('click', function(e) {
+    // document.getElementsById('myvideo').src = context;
+    // console.log("gif:" + gif);
+    let v = {
+      image: gif
+    }
+
+    console.log(v);
+    socket.emit('image', v);
+    //v returns array of images
+    gif = [];
+  });
+
+
+  // Listen for img from partners
+  socket.on('img', function(data) {
+    displayImage(data);
+  });
+
+});
+
+
+
+
+///////////////* DUMPSTER CODE *///////////////
+/* FROM SHAWN EX
+var sendmessage = function () {
+    var message = document.getElementById('message').value;
+    console.log("Sending: " + message);
+
+    // Send a messaage
+    socket.send(message);
+};
+
+var sendother = function () {
+    var othermessage = document.getElementById('message').value;
+    console.log("sending: " + othermessage);
+
+    // Send any kind of data with a custom event
+    //socket.emit('otherevent',{ othermessage: othermessage });
+    socket.emit('otherevent', othermessage);
+};
+*/
+
+
+/* FROM SHAWN EX
+//listen for a message
+socket.on('message', function (data) {
+    console.log("Got: " + data);
+    document.getElementById('messages').innerHTML += data;
+});
+*/
---
+++ 4_week/gif_chat/server.js
@@ -0,0 +1,148 @@
+// Create server
+let port = process.env.PORT || 8000;
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app).listen(port, function() {
+  console.log('Server listening at port: ', port);
+});
+
+// Tell server where to look for files
+app.use(express.static('public'));
+
+//SOCKET CONNECTION + ROOMS
+// Create socket connection
+let io = require('socket.io').listen(server);
+
+// Get the array of rooms
+let rooms = io.sockets.adapter.rooms;
+let roomNum = 0;
+// How many in a group? Default is 2
+let NUM_PARTNERS = 2;
+
+
+// Listen for clients to connect
+io.sockets.on('connection',
+
+  function(socket) {
+    console.log('An input client connected: ' + socket.id);
+
+    // Join a room
+    joinRoom(socket);
+
+    socket.on('username', function(senderUsername) {
+
+      let room = socket.room;
+      let data = senderUsername;
+      // console.log(data);
+      let members = rooms[room].sockets;
+      // console.log(members);
+      let sender = socket.id;
+      // console.log('sender: ' + sender);
+      let receiver;
+      for (member in members) {
+        if (member != sender) {
+          receiver = member;
+          // console.log('receiver: ' + receiver);
+        }
+      }
+
+      // socket.to(room).emit('connectedUsername', data);
+      socket.broadcast.to(receiver).emit('connectedUsername', data);
+    });
+
+
+    /* IGNORE TEXT MSG FOR NOW
+    // Listen for data messages
+    socket.on('text', function (data) {
+      // Data comes in as whatever was sent, including objects
+      //console.log("Received: 'message' " + data);
+      // Which private room does this client belong to?
+      let room = socket.room;
+
+      // Share data to all members of room
+      socket.to(room).emit('text', data);
+    }
+*/
+
+    /////// MERGED SECTION ///////
+
+    // listen for 'image' event then emit to other sockets
+    socket.on('image', function(data) {
+      let room = socket.room;
+      // console.log(data);
+      let members = rooms[room].sockets;
+      // console.log(members);
+      let sender = socket.id;
+      // console.log('sender: ' + sender);
+      let receiver;
+      for (member in members) {
+        if (member != sender) {
+          receiver = member;
+          // console.log('receiver: ' + receiver);
+        }
+      }
+
+      socket.broadcast.to(receiver).emit('image', data);
+    });
+
+    // listen for 'position' event then emit to other sockets
+    socket.on('position', function(data) {
+      socket.emit('position', data);
+    })
+
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('otherevent', function(data) {
+      // Data comes in as whatever was sent, including objects
+      console.log("Received: 'otherevent' " + data);
+    });
+
+    /*
+    //SAY WHO YOU ARE PARTNERED WITH
+    socket.on('istyping', function () {
+      let room = socket.room;
+      socket.to(room).emit('istyping');
+    });
+    */
+
+    //SAY WHEN PARTNER LEFT ROOM
+    // Listen for this client to disconnect
+    // Tell partners this client disconnected
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected " + socket.id);
+
+      // Which room was this client in?
+      let room = socket.room;
+      // Tell others in room client has left
+      if (rooms[room]) {
+        socket.to(room).emit('leave room');
+      }
+
+    });
+  });
+
+
+// Join room
+function joinRoom(socket) {
+  // First, add client to incomplete rooms
+  for (let r in rooms) {
+    let room = rooms[r];
+    if (room.isPrivate) {
+      if (room.length < NUM_PARTNERS) {
+        addSocketToRoom(socket, r);
+        return;
+      }
+    }
+  }
+
+  // If there are no incomplete rooms, create new room and join it
+  addSocketToRoom(socket, roomNum);
+  roomNum++;
+}
+
+// Add client to room and record which room it was added to
+function addSocketToRoom(socket, r) {
+  socket.join(r);
+  rooms[r].isPrivate = true;
+  socket.room = r;
+  console.log(rooms);
+}
